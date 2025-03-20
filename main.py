25178d1c102330df4a7dd37d8ecc7547b5c9905e
PedroRamirez01 <85964832+PedroRamirez01@users.noreply.github.com>
2025-03-20 19:08:37 -0300

    bug cerrando pop up

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ from pages.login import LoginPage
 from pages.register import RegisterPage
 from pages.homeToRegister import HomeToRegisterPage
 
-URL = "https://automation-portal-bootcamp.vercel.app/"
+URL = "https://automation-portal-bootcamp.vercel.app/register"
 
-FIRST_NAME = "José"
-LAST_NAME = "Hernández"
-EMAIL = "jh@gmail.com"
-PASSWORD = "jh12345"
+happy_path = [
+    {
+        "FIRST_NAME": "José",
+        "LAST_NAME": "Hernández",
+        "EMAIL": "test1@gmail.com",
+        "PASSWORD": "2&3df5g"
+    }
+]
+
+sad_path = [
+    {
+        "FIRST_NAME": "José",
+        "LAST_NAME": "Hernández",
+        "EMAIL": "jh@gmail.com",
+        "PASSWORD": ""
+    }
+]
+
+path = happy_path
 
 async def main():
     async with async_playwright() as p:
@@ -19,30 +34,31 @@ async def main():
         page = await browser.new_page()
         await page.goto(URL, wait_until="domcontentloaded")
 
-        homeToRegisterPage = HomeToRegisterPage(page)
-        await homeToRegisterPage.closePopUpHomePage()
-        await homeToRegisterPage.pressBtnLogin()
-        await homeToRegisterPage.pressLinkRegister()
-        await homeToRegisterPage.pressBtnRegister()
-        await homeToRegisterPage.closePopUpRegister()
-
-        time.sleep(2)
+        # homeToRegisterPage = HomeToRegisterPage(page)
+        # await homeToRegisterPage.closePopUpHomePage()
+        # await homeToRegisterPage.pressBtnLogin()
+        # await homeToRegisterPage.pressLinkRegister()
+        # await homeToRegisterPage.pressBtnRegister()
+        # await homeToRegisterPage.closePopUpRegister()
+        # time.sleep(2)
 
         registerPage = RegisterPage(page)
-        await registerPage.registerFirstName(FIRST_NAME)
-        await registerPage.registerLastName(LAST_NAME)
-        await registerPage.registerEmail(EMAIL)
-        await registerPage.registerPassword(PASSWORD)
+        await registerPage.registerFirstName(path[0]["FIRST_NAME"])
+        await registerPage.registerLastName(path[0]["LAST_NAME"])
+        await registerPage.registerEmail(path[0]["EMAIL"])
+        await registerPage.registerPassword(path[0]["PASSWORD"])
         await registerPage.pressBtnRegister()
 
-        time.sleep(2)
+        time.sleep(5)
 
         loginPage = LoginPage(page)
-        await loginPage.loginEmail(EMAIL)
-        await loginPage.loginPassword(PASSWORD)
+        await loginPage.loginEmail(path[0]["EMAIL"])
+        await loginPage.loginPassword(path[0]["PASSWORD"])
         await loginPage.pressBtnLogin()
 
-        time.sleep(10)
+        time.sleep(5)
+
+        await browser.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
