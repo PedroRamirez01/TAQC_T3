1d6ee31be27e0f4722a488950cb9a37f44335bce
PedroRamirez01 <85964832+PedroRamirez01@users.noreply.github.com>
2025-04-08 15:23:48 -0400

    refactor: update login and registration tests to use USER constants

--- login/test/test_login.py
+++ login/test/test_login.py
@@ -3,17 +3,17 @@ from config.config import Config
 
 @pytest.mark.asyncio
 async def test_successful_login(login_page):
-    await login_page.login(Config.EMAIL, Config.PASSWORD)
+    await login_page.login(Config.USER_EMAIL, Config.USER_PASSWORD)
     assert "/my-account" in login_page.page.url
 
 @pytest.mark.asyncio
 async def test_login_with_empty_email(login_page):
-    await login_page.login("", Config.PASSWORD)
+    await login_page.login("", Config.USER_PASSWORD)
     assert await login_page.get_field_validation_state("email") is True
 
 @pytest.mark.asyncio
 async def test_login_with_empty_password(login_page):
-    await login_page.login(Config.EMAIL, "")
+    await login_page.login(Config.USER_EMAIL, "")
     assert await login_page.get_field_validation_state("password") is True
 
 @pytest.mark.asyncio
--- login/test/test_register.py
+++ login/test/test_register.py
@@ -1,52 +1,52 @@
 import pytest
 from config.config import Config
 
-# @pytest.mark.asyncio
-# async def test_successful_registration(register_page):
-#     await register_page.register(
-#         Config.NEW_USER_FIRST_NAME,
-#         Config.NEW_USER_LAST_NAME,
-#         Config.NEW_USER_EMAIL,
-#         Config.NEW_USER_PASSWORD
-#         )
-#     assert "/my-account" in register_page.page.url
+@pytest.mark.asyncio
+async def test_successful_registration(register_page):
+    await register_page.register(
+        Config.USER_FIRST_NAME,
+        Config.USER_LAST_NAME,
+        Config.USER_EMAIL,
+        Config.USER_PASSWORD
+        )
+    assert "/my-account" in register_page.page.url
 
 @pytest.mark.asyncio
 async def test_registration_with_empty_first_name(register_page):
     await register_page.register(
         "",
-        Config.NEW_USER_LAST_NAME,
-        Config.NEW_USER_EMAIL,
-        Config.NEW_USER_PASSWORD
+        Config.USER_LAST_NAME,
+        Config.USER_EMAIL,
+        Config.USER_PASSWORD
         )
     assert await register_page.get_field_validation_state("firstName") is True
 
 @pytest.mark.asyncio
 async def test_registration_with_empty_last_name(register_page):
     await register_page.register(
-        Config.NEW_USER_FIRST_NAME,
+        Config.USER_FIRST_NAME,
         "",
-        Config.NEW_USER_EMAIL,
-        Config.NEW_USER_PASSWORD
+        Config.USER_EMAIL,
+        Config.USER_PASSWORD
         )
     assert await register_page.get_field_validation_state("lastName") is True
 
 @pytest.mark.asyncio
 async def test_registration_with_empty_email(register_page):
     await register_page.register(
-        Config.NEW_USER_FIRST_NAME,
-        Config.NEW_USER_LAST_NAME,
+        Config.USER_FIRST_NAME,
+        Config.USER_LAST_NAME,
         "",
-        Config.NEW_USER_PASSWORD
+        Config.USER_PASSWORD
         )
     assert await register_page.get_field_validation_state("email") is True
 
 @pytest.mark.asyncio
 async def test_registration_with_empty_password(register_page):
     await register_page.register(
-        Config.NEW_USER_FIRST_NAME,
-        Config.NEW_USER_LAST_NAME,
-        Config.NEW_USER_EMAIL,
+        Config.USER_FIRST_NAME,
+        Config.USER_LAST_NAME,
+        Config.USER_EMAIL,
         ""
         )
     assert await register_page.get_field_validation_state("password") is True
@@ -54,29 +54,19 @@ async def test_registration_with_empty_password(register_page):
 @pytest.mark.asyncio
 async def test_registration_with_invalid_email(register_page):
     await register_page.register(
-        Config.NEW_USER_FIRST_NAME,
-        Config.NEW_USER_LAST_NAME,
+        Config.USER_FIRST_NAME,
+        Config.USER_LAST_NAME,
         Config.INVALID_EMAIL,
-        Config.NEW_USER_PASSWORD
+        Config.USER_PASSWORD
         )
     assert await register_page.get_field_validation_state("email") is True
-
-# @pytest.mark.asyncio
-# async def test_registration_with_short_password(register_page):
-#     await register_page.register(
-#         Config.NEW_USER_FIRST_NAME,
-#         Config.NEW_USER_LAST_NAME,
-#         Config.NEW_USER_EMAIL,
-#         Config.INVALID_PASSWORD
-#         )
-#     assert await register_page.get_field_validation_state("password") is True
 
 @pytest.mark.asyncio
 async def test_registration_whit__existing_email(register_page):
     await register_page.register(
-        Config.NEW_USER_FIRST_NAME,
-        Config.NEW_USER_LAST_NAME,
-        Config.EMAIL,
-        Config.PASSWORD
+        Config.USER_FIRST_NAME,
+        Config.USER_LAST_NAME,
+        Config.USER_EMAIL,
+        Config.USER_PASSWORD
         )
     assert "/register" in register_page.page.url
