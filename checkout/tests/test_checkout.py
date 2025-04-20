ced472a3e225f04134623fc4a254e7910df12dd7
Catalina <riquelmecata@gmail.com>
2025-04-19 20:20:03 -0400

    dos tests ok

--- checkout/tests/test_checkout.py
+++ checkout/tests/test_checkout.py
@@ -12,56 +12,56 @@ URL = "https://automation-portal-bootcamp.vercel.app/product-detail/1"
 
 test_data = valid_checkout_data + invalid_checkout_data
 
-# @pytest.mark.asyncio
-# @pytest.mark.parametrize("label,data", test_data)
-# async def test_checkout_flow(label,data,page):
-#         await page.goto(URL, wait_until="domcontentloaded")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("label,data", test_data)
+async def test_checkout_flow(label,data,page):
+        await page.goto(URL, wait_until="domcontentloaded")
 
-#         # homeToProductDetails = HomeToProductDetails(page)
-#         # await homeToProductDetails.closePopup()
-#         # await homeToProductDetails.clickFirstCollection()
-#         # await homeToProductDetails.clickFirstProduct()
+        # homeToProductDetails = HomeToProductDetails(page)
+        # await homeToProductDetails.closePopup()
+        # await homeToProductDetails.clickFirstCollection()
+        # await homeToProductDetails.clickFirstProduct()
 
-#         addToCart = AddToCart(page)
-#         await addToCart.changeColor()
-#         await addToCart.changeSize()
-#         await addToCart.incrementQuantity()
-#         await addToCart.incrementQuantity()
-#         await addToCart.incrementQuantity()
-#         await addToCart.incrementQuantity()
-#         await addToCart.incrementQuantity()
-#         await addToCart.decrementQuantity()
-#         await addToCart.addToCart()
+        addToCart = AddToCart(page)
+        await addToCart.changeColor()
+        await addToCart.changeSize()
+        await addToCart.incrementQuantity()
+        await addToCart.incrementQuantity()
+        await addToCart.incrementQuantity()
+        await addToCart.incrementQuantity()
+        await addToCart.incrementQuantity()
+        await addToCart.decrementQuantity()
+        await addToCart.addToCart()
 
-#         checkoutpage = CheckoutPage(page)
-#         await checkoutpage.clickTermsAndConditionsCheckbox()
-#         await checkoutpage.clickProceedToCheckoutButton()
-#         await checkoutpage.fillFirstName(data["FIRST_NAME"])
-#         await checkoutpage.fillLastName(data["LAST_NAME"])
-#         await checkoutpage.fillCountry(data["COUNTRY"])
-#         await checkoutpage.fillCity(data["CITY"])
-#         await checkoutpage.fillAdress(data["ADDRESS"])
-#         await checkoutpage.fillPhoneNumber(data["PHONE_NUMBER"])
-#         await checkoutpage.fillEmail(data["EMAIL"])  
-#         await checkoutpage.fillDiscountCode(data["DISCOUNT_CODE"])
-#         await checkoutpage.fillCardNumber(data["CARD_NUMBER"])
-#         await checkoutpage.fillCardExpiration(data["CARD_EXPIRATION"])
-#         await checkoutpage.fillCardCVV(data["CARD_CVV"])
-#         await checkoutpage.clickAgreeCheckbox()
-#         await checkoutpage.clickPlaceOrderButton()
+        checkoutpage = CheckoutPage(page)
+        await checkoutpage.clickTermsAndConditionsCheckbox()
+        await checkoutpage.clickProceedToCheckoutButton()
+        await checkoutpage.fillFirstName(data["FIRST_NAME"])
+        await checkoutpage.fillLastName(data["LAST_NAME"])
+        await checkoutpage.fillCountry(data["COUNTRY"])
+        await checkoutpage.fillCity(data["CITY"])
+        await checkoutpage.fillAdress(data["ADDRESS"])
+        await checkoutpage.fillPhoneNumber(data["PHONE_NUMBER"])
+        await checkoutpage.fillEmail(data["EMAIL"])  
+        await checkoutpage.fillDiscountCode(data["DISCOUNT_CODE"])
+        await checkoutpage.fillCardNumber(data["CARD_NUMBER"])
+        await checkoutpage.fillCardExpiration(data["CARD_EXPIRATION"])
+        await checkoutpage.fillCardCVV(data["CARD_CVV"])
+        await checkoutpage.clickAgreeCheckbox()
+        await checkoutpage.clickPlaceOrderButton()
 
-#         await page.wait_for_timeout(5000) 
+        await page.wait_for_timeout(5000) 
 
-#         # Los label con missing pasan, ya que no se hace la compra, pero deberian mostrar un mensaje de "invalid input ??"
+        # Los label con missing pasan, ya que no se hace la compra, pero deberian mostrar un mensaje de "invalid input ??"
 
-#         success_message = page.locator("#wrapper > section > div > div > div.tf-page-cart-footer > div > form > p:nth-child(9)")
+        success_message = page.locator("#wrapper > section > div > div > div.tf-page-cart-footer > div > form > p:nth-child(9)")
 
-#         if label.startswith("valid"):
-#             assert await success_message.is_visible(), f"[{label}] Expected success message not found."
-#         else:
-#             assert not await success_message.is_visible(), f"[{label}] Unexpected success message for invalid input."
+        if label.startswith("valid"):
+            assert await success_message.is_visible(), f"[{label}] Expected success message not found."
+        else:
+            assert not await success_message.is_visible(), f"[{label}] Unexpected success message for invalid input."
 
-#         #empty cart 
+        #empty cart 
 
 
 @pytest.mark.asyncio
