c8fa98e677c5993b7b82bdfa46f4813a140e81d5
Catalina <riquelmecata@gmail.com>
2025-04-28 17:23:11 -0400

    changes on checkout comments

--- ecomus/pages/addToCart.py
+++ ecomus/pages/addToCart.py
@@ -3,25 +3,15 @@ from playwright.async_api import Page
 class AddToCart:
 
     """
-    This class is used to interact with the add to cart page of the website.
-    It contains methods to change the color and size of the product, increment and decrement the quantity, and add the product to the cart.
-    atributes:
-    - page: The page object of the website.
-    - changeColorButton: The button to change the color of the product.
-    - changeSizeButton: The button to change the size of the product.
-    - IncrementButton: The button to increment the quantity of the product.
-    - DecrementButton: The button to decrement the quantity of the product
-    - addToCartButton: The button to add the product to the cart.
-
-    methods:
-    - changeColor: This method is used to change the color of the product.
-    - changeSize: This method is used to change the size of the product.    
-    - incrementQuantity: This method is used to increment the quantity of the product.
-    - decrementQuantity: This method is used to decrement the quantity of the product.
-    - addToCart: This method is used to add the product to the cart.
+    Page Object Model for the add-to-cart page.
+    Allows changing product color and size, modifying quantity, and adding products to the cart.
     """
     
     def __init__(self, page: Page):
+        """
+        Initializes locators for the add-to-cart functionalities.
+        :param page: Instance of Playwright Page.
+        """
         self.page = page
         self.changeColorButton = self.page.locator('#wrapper > section:nth-child(3) > div.tf-main-product.section-image-zoom > div > div > div:nth-child(2) > div > div.tf-product-info-list.other-image-zoom > div.tf-product-info-variant-picker > div:nth-child(1) > form > label:nth-child(4)')
         self.changeSizeButton = self.page.locator('#wrapper > section:nth-child(3) > div.tf-main-product.section-image-zoom > div > div > div:nth-child(2) > div > div.tf-product-info-list.other-image-zoom > div.tf-product-info-variant-picker > div:nth-child(2) > form > label:nth-child(6)')
@@ -30,22 +20,37 @@ class AddToCart:
         self.addToCartButton = self.page.locator('#wrapper > section:nth-child(3) > div.tf-main-product.section-image-zoom > div > div > div:nth-child(2) > div > div.tf-product-info-list.other-image-zoom > div.tf-product-info-buy-button > form > a.tf-btn.btn-fill.justify-content-center.fw-6.fs-16.flex-grow-1.animate-hover-btn')
 
     async def changeColor(self):
+        """
+        Changes the product color.
+        """
         assert self.changeColorButton, "No color button found"
         await self.changeColorButton.click()
 
     async def changeSize(self):
+        """
+        Changes the product size.
+        """
         assert self.changeSizeButton, "No size button found"
         await self.changeSizeButton.click()
 
     async def incrementQuantity(self):
+        """
+        Increases the product quantity.
+        """
         assert self.IncrementButton, "No increment button found"
         await self.IncrementButton.click()
 
     async def decrementQuantity(self):
+        """
+        Decreases the product quantity.
+        """
         assert self.DecrementButton, "No decrement button found"
         await self.DecrementButton.click()
 
     async def addToCart(self):
+        """
+        Adds the product to the shopping cart.
+        """
         assert self.addToCartButton, "No add to cart button found"
         await self.addToCartButton.click()
         
